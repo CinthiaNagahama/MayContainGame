345636e357557161b6a94a5640934c96e3ec24cb
CinthiaNagahama <56683006+CinthiaNagahama@users.noreply.github.com>
2020-10-27 15:49:43 -0300

    F

--- Main.c
+++ Main.c
@@ -3,22 +3,22 @@
 #include "Screen.h"
 
 /* Só pro screenshot */
-							qtd = mao->qtd_cartas;
-                            while(!mao_vazia(mao)){
-                                mao_para_deck(deck, mao, 0);
-                                (...)
-                                SDL_RenderPresent(gRenderer);
-                                SDL_Delay(100);
-                            }
-                            embaralha_deck(deck);
-                            while(qtd > 0){
-                                compra_carta(deck, mao);
-                                (...)
-                                SDL_RenderPresent(gRenderer);
-                                SDL_Delay(100);
-                                qtd--;
-                            }
-                            inimigos[i].turno = 0;
+qtd = mao->qtd_cartas;
+while(!mao_vazia(mao)){
+	mao_para_deck(deck, mao, 0);
+	(...)
+	SDL_RenderPresent(gRenderer);
+	SDL_Delay(100);
+}
+embaralha_deck(deck);
+while(qtd > 0){
+    compra_carta(deck, mao);
+    (...)
+    SDL_RenderPresent(gRenderer);
+    SDL_Delay(100);
+    qtd--;
+}
+inimigos[i].turno = 0;
                             
 
 int main(void){
