41810b7567b2e3e780307db2569070872f29bdd2
CinthiaNagahama <56683006+CinthiaNagahama@users.noreply.github.com>
2020-10-27 15:47:04 -0300

    ScreeShot

--- Main.c
+++ Main.c
@@ -2,6 +2,26 @@
 #include <locale.h>
 #include "Screen.h"
 
+/* Só pro screenshot */
+
+							qtd = mao->qtd_cartas;
+                            while(!mao_vazia(mao)){
+                                mao_para_deck(deck, mao, 0);
+                                (...)
+                                SDL_RenderPresent(gRenderer);
+                                SDL_Delay(100);
+                            }
+                            embaralha_deck(deck);
+                            while(qtd > 0){
+                                compra_carta(deck, mao);
+                                (...)
+                                SDL_RenderPresent(gRenderer);
+                                SDL_Delay(100);
+                                qtd--;
+                            }
+                            inimigos[i].turno = 0;
+                            
+
 int main(void){
 	setlocale(LC_ALL, "Portuguese");
 	print_screen("Historia/DIA 1/BLOCO A.txt", 1);
